280e5cc5937e370a03c3d25de1224e36ab563c10
ekaterinasidelnikova <ekaterina.sidelnikova@yandex.ru>
2022-12-27 16:30:15 +0100

    Login form added

--- math_net/math_net_app/urls.py
+++ math_net/math_net_app/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from math_net_app import views
 
 urlpatterns = [
-    path('', views.home, name="home"),
+    path('', views.login, name="login"),
     path('about/', views.about, name="about")
 ]
--- math_net/math_net_app/views.py
+++ math_net/math_net_app/views.py
@@ -1,9 +1,32 @@
 from django.shortcuts import render
 import connect
+from .forms import LoginForm
+
 ms = connect.MongoService()
+
+
 # Create your views here.
 def home(request):
     return render(request, 'math_net_app/index.html', {'accs': ms.get_users()})
 
+
 def about(request):
+    try:
+        request.session.pop("username")
+    except KeyError:
+        print("Arleady no session!!!")
     return render(request, 'math_net_app/about.html', {'num': range(10000)})
+
+
+def login(request):
+    print("String 13,", request)
+    form = LoginForm(request.POST or None)
+    if form.is_valid():
+        request.session['username'] = form.cleaned_data.get("user_name")
+    sssr = request.session.get("username", None)
+    if sssr is None:
+        # Redirecting to login page
+        return render(request, 'math_net_app/login.html', {"form": form})
+    else:
+        # Redirecting to home page
+        return home(request)
